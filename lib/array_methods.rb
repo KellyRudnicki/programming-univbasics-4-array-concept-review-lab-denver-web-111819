96d34021a1fc4f661f6baf6910a05a988662e36b
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-07 21:56:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
 
   array.length.times do
-   if array[counter] == value_to_find
-  return counter
+   if array[count] == value_to_find
+  return count
   
 end 
 end 
